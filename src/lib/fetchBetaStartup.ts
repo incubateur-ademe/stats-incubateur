c2918fad1aa8e2ddcd22c1d510e114834dc023ec
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-12-03 15:49:06 +0100

    fix(betagouv): switch to new api route to fetch startups

--- src/lib/fetchBetaStartup.ts
+++ src/lib/fetchBetaStartup.ts
@@ -2,10 +2,11 @@ import { config } from "@/config";
 import { type BetaGouvStartup } from "@/startup-types";
 
 export const fetchBetaStartup = async (id: string) => {
-  const res = await fetch(`${config.betaGouvUrl}/startups/${id}.json`, {
+  const res = await fetch(`${config.betaGouvUrl}/api/v3/startups/${id}.json`, {
     next: { revalidate: 3600 }, // 1 hour
   });
   if (!res.ok) {
+    console.log("======", `${config.betaGouvUrl}/api/startups/${id}.json`);
     throw new Error(`Failed to fetch startup data for id ${id}: ${res.statusText}`);
   }
   return res.json() as Promise<BetaGouvStartup>;
